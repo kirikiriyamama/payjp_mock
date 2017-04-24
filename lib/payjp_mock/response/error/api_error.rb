77cb465f18a41c105f724682a26fa18bb0aa0943
kirikiriyamama <kirikiriyamama@gmail.com>
2017-04-25 02:58:16 +0900

    Make APIError realistic

--- lib/payjp_mock/response/error/api_error.rb
+++ lib/payjp_mock/response/error/api_error.rb
@@ -2,14 +2,15 @@ module PayjpMock::Response::Error
   class ApiError < Base
     def default_attributes
       {
-        message: 'API server is currently unavailable',
+        code:    'under_maintenance',
+        message: 'PAY.JP is currently undergoing maintenance. Please try again later.',
         status:  status,
         type:    'server_error'
       }
     end
 
     def status
-      500
+      503
     end
   end
 end
