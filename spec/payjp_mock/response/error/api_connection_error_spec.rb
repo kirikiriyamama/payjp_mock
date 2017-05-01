adb6c75f5cc5fd70a6dd8b182f8621f02aa77211
kirikiriyamama <kirikiriyamama@gmail.com>
2017-05-01 23:41:22 +0900

    Add spec for ApiConnectionError

--- spec/payjp_mock/response/error/api_connection_error_spec.rb
+++ spec/payjp_mock/response/error/api_connection_error_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe PayjpMock::Response::Error::ApiConnectionError do
   specify do
     error = described_class.new
 
+    expect(error.attributes).to be_nil
     expect(error.body).to be_nil
     expect(error.status).to be_nil
     expect(error.exception).to eq SocketError
